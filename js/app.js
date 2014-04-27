38df9112bc0011e2e2373bb08f2607762b5b3ce6
Pilot Interactive Inc <david@pilotinteractive.ca>
2014-04-27 12:17:59 -0400

    Patched as much mobile as I could.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
 "use strict";
 
 $(document).ready(function() {
+
+	var years = {};
+	var currYear = '1986';
+
+	// Initialize animation Tweens
+
 	TweenMax.to(
 		$('.droplet.first'),
 		1.5,
@@ -20,27 +26,38 @@ $(document).ready(function() {
 			"ease": Linear.easeNone
 		}
 	);
-	var years = {};
-	var currYear = '1986';
+
 	$('[data-year]').each(function(index, el) {
 		var offset = $(el).offset()
 		years[offset.top] = $(el).data('year');
 	});
+
+	// Initialize the scroll library
+
 	var s = skrollr.init({
-	    render: function(data) {
-	    	console.log(data.curTop);
-	        for (var i in years) {
-	        	if (i < data.curTop + 250) {
-	        		currYear = years[i];
-	        	} else {
-	        		break;
-	        	}
-	        }
-	        $('nav .year').removeClass('active');
-	        $('nav .year.' + currYear).addClass('active');
-	    }
+    render: function(data) {
+      for (var i in years) {
+      	if (i < data.curTop + 250) {
+      		currYear = years[i];
+      	} else {
+      		break;
+      	}
+      }
+      $('nav .year').removeClass('active');
+      $('nav .year.' + currYear).addClass('active');
+    }
 	});
+
+	// Handle mobile menu clicks
+
+	$('.small').on('click', function(e) {
+		$('.big').toggleClass('active');
+	});
+
+	// Handle clicks on timeline
+
 	$('nav .year').on('click', function(e) {
+		$('.big').removeClass('active');
 		var currYear = $(e.currentTarget).find('span').html();
 		var offset = $('[data-year="' + currYear + '"]').offset();
 		s.animateTo(offset.top, {
