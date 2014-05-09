22223c8759a837abcdabbce6b441dab881946024
Pilot Interactive Inc <david@pilotinteractive.ca>
2014-05-09 13:56:30 -0400

    Patched line below video.

--- js/app.js
+++ js/app.js
@@ -3,80 +3,80 @@
 
 $(document).ready(function() {
 
-	var years = {};
-	var currYear = '1986';
+  var years = {};
+  var currYear = '1986';
 
-	// Initialize animation Tweens
+  // Initialize animation Tweens
 
-	TweenMax.to(
-		$('.droplet.first'),
-		1.5,
-		{
-			"backgroundPosition": "0px 70px",
-			"opacity": 0,
-			"repeat": -1
-		}
-	);
-	TweenMax.to(
-		$('.zah'),
-		3,
-		{
-			"rotationY": "360deg",
-			"repeat": -1,
-			"ease": Linear.easeNone
-		}
-	);
+  TweenMax.to(
+    $('.droplet.first'),
+    1.5,
+    {
+      "backgroundPosition": "0px 70px",
+      "opacity": 0,
+      "repeat": -1
+    }
+  );
+  TweenMax.to(
+    $('.zah'),
+    3,
+    {
+      "rotationY": "360deg",
+      "repeat": -1,
+      "ease": Linear.easeNone
+    }
+  );
 
-	$('[data-year]').each(function(index, el) {
-		var offset = $(el).offset()
-		years[offset.top] = $(el).data('year');
-	});
+  $('[data-year]').each(function(index, el) {
+    var offset = $(el).offset()
+    years[offset.top] = $(el).data('year');
+  });
 
-	// Initialize the scroll library
+  // Initialize the scroll library
 
-	var s = skrollr.init({
+  var s = skrollr.init({
     render: function(data) {
       for (var i in years) {
-      	if (i < data.curTop + 250) {
-      		currYear = years[i];
-      	} else {
-      		break;
-      	}
+        if (i < data.curTop + 250) {
+          currYear = years[i];
+        } else {
+          break;
+        }
       }
       $('nav .year').removeClass('active');
       $('nav .year.' + currYear).addClass('active');
     }
-	});
+  });
 
-	// Handle mobile menu clicks
+  // Handle mobile menu clicks
 
-	$('.small').on('click', function(e) {
-		$('.big').toggleClass('active');
-	});
+  $('.small').on('click', function(e) {
+    $('.big').toggleClass('active');
+  });
 
-	// Handle clicks on timeline
+  // Handle clicks on timeline
 
-	$('nav .year').on('click', function(e) {
-		$('.big').removeClass('active');
-		var currYear = $(e.currentTarget).find('span').html();
-		var offset = $('[data-year="' + currYear + '"]').offset();
-		s.animateTo(offset.top, {
-			duration: 1250
-		});
-	});
+  $('nav .year').on('click', function(e) {
+    $('.big').removeClass('active');
+    var currYear = $(e.currentTarget).find('span').html();
+    var offset = $('[data-year="' + currYear + '"]').offset();
+    s.animateTo(offset.top, {
+      duration: 1250
+    });
+  });
 
-	$(window).resize(function() {
-		$('video').width($(window).width());
-	});
+  $(window).resize(function() {
     $('video').width($(window).width());
+  });
+  $('video').width($(window).width());
 
-	var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/g);
-	if(! isMobile) {
-	    // User-Agent is not IPhone, IPod, IPad, Android or BlackBerry
-	    $('video').html('<video loop="1" muted autoplay="autoplay" poster="http://therapup.net/wp-content/uploads/2013/04/Puff.gif">'
-	    + '<source src="video.webm" type="video/webm">'+' <source src="video.m4v">'+' <source src="video/dancing.mp4" type="video/mp4">' + '</video>');
-	} else {
-	    $('video').html('<img src="http://therapup.net/wp-content/uploads/2013/04/Puff.gif">');
-	}
+  var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/g);
+  if(! isMobile) {
+    // User-Agent is not IPhone, IPod, IPad, Android or BlackBerry
+    $('video').html('<video loop="1" muted autoplay="autoplay" poster="http://therapup.net/wp-content/uploads/2013/04/Puff.gif">'
+    + '<source src="video.webm" type="video/webm">'+' <source src="video.m4v">'+' <source src="video/dancing.mp4" type="video/mp4">' + '</video>');
+  } else {
+    $('video').html('<img src="http://therapup.net/wp-content/uploads/2013/04/Puff.gif">');
+  }
 
 });
